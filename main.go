42cb76a57d9fa2f181136b89bc2325a4f2b3d995
aman-zulfiqar <amanzulfiqar75@gmail.com>
2025-06-16 23:54:16 +0500

    Main file

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-
-	function "github.com/aman-zulfiqar/MusicList-API-Task5/functions"
-	"github.com/aman-zulfiqar/MusicList-API-Task5/models"
-	"github.com/aman-zulfiqar/MusicList-API-Task5/config"
+	"github.com/aman-zulfiqar/MusicList-API/config"
+	functions "github.com/aman-zulfiqar/MusicList-API/functions"
+	"github.com/aman-zulfiqar/MusicList-API/models"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -12,33 +11,28 @@ import (
 )
 
 func init() {
-	log.SetFormatter(&log.TextFormatter{
-		FullTimestamp: true,
-	})
+	log.SetFormatter(&log.TextFormatter{FullTimestamp: true})
 }
 
 func main() {
-
 	config.InitDB()
-
-	// Auto-migrate Song model
+	
 	if err := config.DB.AutoMigrate(&models.Song{}); err != nil {
 		log.Fatalf("Migration failed: %v", err)
 	}
 
 	e := echo.New()
-
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/", function.HomeHandler)
-	e.POST("/songs", function.CreateSongHandler)
-	e.GET("/songs", function.FetchSongsHandler)
-	e.PUT("/songs/:id", function.UpdateSongHandler)
-	e.DELETE("/songs/:id", function.DeleteSongHandler)
+	e.GET("/", functions.HomeHandler)
+	e.POST("/songs", functions.CreateSongHandler)
+	e.GET("/songs", functions.FetchSongsHandler)
+	e.PUT("/songs/:id", functions.UpdateSongHandler)
+	e.DELETE("/songs/:id", functions.DeleteSongHandler)
 
 	log.Info("Music Playlist API is starting on :9010")
 	if err := e.Start(":9010"); err != nil {
-		log.Fatalf("server failed to start: %v", err)
+		log.Fatalf("Server failed to start: %v", err)
 	}
 }
